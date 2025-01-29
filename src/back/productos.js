55b60d2d70ee926d89673a16b21de403f4e282fc
matisosagit <matiassosa1314@gmail.com>
2025-01-29 17:07:29 -0300

    arreglo de modelo Producto y avance en el ruteo

--- src/back/productos.js
+++ src/back/productos.js
@@ -1,35 +1,94 @@
-import expressRouter from "express-router";
+import express from "express";
 import { Sequelize, DataTypes } from "sequelize";
-import con from './conbbdd';
-import { request } from "express";
+import con from './conbbdd.js';
+
+
+const app = express();
+const ruteo = express.Router();
+
 
 (async() => {
-    await con();}
-);
-
-const ruteo = expressRouter();
-
-const Producto = Sequelize.define(
-    'Producto', {
-        nombre : {
-            type : DataTypes.STRING,
-            allowNull : false
-        },
-        descripcion : {
-            type: DataTypes.STRING
-        },
-        precio : {
-            type : DataTypes.INTEGER
-        }
-    });
-
-(async() => {
-    await Producto.sync();
+    const sequelize = await con();
+    const Producto = sequelize.define(
+        'producto', {
+            nombre : {
+                type : DataTypes.STRING,
+                allowNull : false
+            },
+            descripcion : {
+                type: DataTypes.STRING
+            },
+            precio : {
+                type : DataTypes.INTEGER
+            },
+            categoria: {
+                type: DataTypes.STRING
+            }
+        });
+        await Producto.sync();
 });
 
 ruteo.post('/nuevoProducto', async(request, res) => {
     const {nombreI, descripcionI, precioI} = request.body;
-    //try para crear producto con sequelize.create
+    try{
+        const nuevoProducto = await Producto.create(
+            {
+                nombre : nombreI,
+                descripcion : descripcionI,
+                precio: precioI
+            }
+        )
+    } catch(error){
+        console.log('Error al crear producto: ', error);
+    }
+});
+
+ruteo.put('/editarProducto', async(request, res) => {
+    const{nombreE, descripcionE, precioE} = request.body;
+    const id = request.id;
+    try{
+        const productoEditado = await Producto.update(
+            {
+                nombre: nombreE,
+                descripcion: descripcionE,
+                precio : precioE
+            },
+            {where: {id : id}}
+        );
+    } catch(error){
+        console.log('Error al actualizar producto: ', error);
+    }
+});
+
+ruteo.delete('/borrarProducto', async(request)=>{
+    const id = request.id;
+    try{
+        await Producto.destroy({where: {id : id}});
+    } catch(error){
+        console.log('Error al borrar producto: ',error);
+    }
+});
+
+ruteo.get('/listaProductos', async(res) => {
+    try{
+        const listaDeProductos = await Producto.findAll();
+        res.status(200).json({message:'proceso exitoso', listaDeProductos});
+    }catch(error){
+        console.log('Error al buscar productos', error);
+        res.status(500).json({error: 'Error al obtener lista de productos'});
+    }
+});
+
+ruteo.get('/listaCategoria/:categoria', async(request,res) => {
+    const categoria = request.params;
+    try{
+        const listaPorCategoria = await Producto.findAll({
+            where:{categoria : categoria}
+        });
+        res.status(200).json({message: 'Proceso exitoso', listaPorCategoria});
+    }catch(error){
+        res.status(500).json({error: 'error al buscar por categoria'});
+    }
 });
 
 
